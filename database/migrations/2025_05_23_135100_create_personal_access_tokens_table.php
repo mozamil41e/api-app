c12bac73b45f870878bcf681ba749d0fd7497a80
mozamil41e <154569643+mozamil41e@users.noreply.github.com>
2025-05-23 17:59:31 +0300

    Update 2025_05_23_135100_create_personal_access_tokens_table.php

--- database/migrations/2025_05_23_135100_create_personal_access_tokens_table.php
+++ database/migrations/2025_05_23_135100_create_personal_access_tokens_table.php
@@ -11,6 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('tokenable');
+            $table->string('name');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamps();
+        });
+    }
+ public function up(): void
+    {
         Schema::create('post', function (Blueprint $table) {
             $table->id();
             $table->string('description');
@@ -18,12 +31,12 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
         Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('post');
     }
 };
