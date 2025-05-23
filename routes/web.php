7211e017be8fe1e47edfbd0c7b6a69c544162a99
mozamil41e <154569643+mozamil41e@users.noreply.github.com>
2025-05-23 18:26:47 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,20 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('/post/{pos}', function ($pos) {
+    $faker = Faker::create();
+    $posts = [];
+    for ($i=0; $i < $pos ; $i++) { 
+        array_push($posts,[
+            "name" => $faker->name ,
+            "desc" => $faker->text,
+            "date" => $faker->date
+        ]);
+    }
+    return response()->json($posts);
+});
+
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
