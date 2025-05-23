3d7bc0cb5b3d2fb990ee7e924016a41d8c980606
mozamil41e <mozamil41e@gmail.com>
2025-05-23 17:50:12 +0300

    poug api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Faker\Factory as Faker;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/post/{pos}', function ($pos) {
+    $faker = Faker::create();
+    $posts = [];
+    for ($i=0; $i < $pos ; $i++) { 
+        array_push($posts,[
+            "name" => $faker->name ,
+            "desc" => $faker->text,
+            "date" => $faker->date
+        ]);
+    }
+    return response()->json($posts);
+});
+
